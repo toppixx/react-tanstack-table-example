784cbf42136ab0cdd12dd01b157b9228ac3972b7
Tobias Faller <TobiasFaller@gmx.de>
2022-08-19 17:39:41 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import MyTable from './Components/BootstrapTable/MyTable';
+
+function App() {
+  return (
+    <div className="App">
+      <MyTable></MyTable>
+    </div>
+  );
+}
+
+export default App;
